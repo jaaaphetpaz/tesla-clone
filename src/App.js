918dc89948b9ac61902e75f1d13b645d16174f73
jaaaphetpaz <japhetpaz17@gmail.com>
2023-03-13 13:37:41 -0400

    Background img + Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      <h1>Tesla Clone</h1>
+    <div className="bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/9160c5a3-b818-42dd-bb98-b8597948c636/bvlatuR/std/1927x4096/M3-Homepage-Mobile-LHD')] lg:bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/a269d7b4-87a9-4057-9ac5-2565e56a188e/bvlatuR/std/4096x2560/Homepage-Model-3-Desktop-LHD')] h-screen bg-cover bg-center">
+      <Navbar />
     </div>
   );
 }
