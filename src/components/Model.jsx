687c0989c995d7a3b3e7355dfa0322f12ba26e88
jaaaphetpaz <japhetpaz17@gmail.com>
2023-03-16 14:36:39 -0400

    FavIcon/Title/Links

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -9,12 +9,16 @@ const Model = () => {
         <p className="p-2 text-lg">Leasing starting at $349/mo</p>
       </div>
       <div className="flex flex-col items-center justify-center text-sm lg:flex-row absolute inset-x-0 bottom-[10%]">
-        <button className="bg-[#393c41] rounded-md w-96 lg:w-64 h-10 lg:mx-4 mt-2 text-white">
-          Custom Order
-        </button>
-        <button className="bg-[#f4f4f4] rounded-md w-96 lg:w-64 h-10 lg:mx-4 mt-2">
-          Demo Drive
-        </button>
+        <a href="https://www.tesla.com/model3/design#overview">
+          <button className="bg-[#393c41] rounded-md w-96 lg:w-64 h-10 lg:mx-4 mt-2 text-white">
+            Custom Order
+          </button>
+        </a>
+        <a href="https://tesla.com/drive">
+          <button className="bg-[#f4f4f4] rounded-md w-96 lg:w-64 h-10 lg:mx-4 mt-2">
+            Demo Drive
+          </button>
+        </a>
       </div>
       <div className="flex justify-center animate-bounce absolute inset-x-0 bottom-[3%]">
         <FiChevronDown size={24} />
